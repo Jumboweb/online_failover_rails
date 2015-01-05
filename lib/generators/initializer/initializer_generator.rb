d31ae907407a13ef35d53c699cdefc473a54b023
Alex <contact@jumboweb.fr>
2015-01-05 13:31:11 +0100

    Add event_bus.rb to initializer generator

--- lib/generators/initializer/initializer_generator.rb
+++ lib/generators/initializer/initializer_generator.rb
@@ -2,5 +2,6 @@ class InitializerGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
   def create_initializer_file
     template 'online_token.rb', File.join('config', 'initializers', 'online_token.rb')
+    template 'event_bus.rb', File.join('config', 'initializers', 'event_bus.rb')
   end
 end
---
+++ lib/generators/initializer/templates/event_bus.rb
@@ -0,0 +1,15 @@
+require 'net/http'
+
+class ErrorRecorder
+  def service_failed(payload)
+    address = payload[:address]
+    #Do something
+
+
+
+  end
+
+  
+end
+
+EventBus.subscribe(ErrorRecorder.new)
